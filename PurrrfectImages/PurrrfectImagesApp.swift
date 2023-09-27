f4467a6e1895a11f3476673f1d6fe41017bd0991
Eslam Nahel <eslamoz.nasser@gmail.com>
2023-09-27 20:22:46 +0200

    Initial Commit

---
+++ PurrrfectImages/PurrrfectImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurrrfectImagesApp.swift
+//  PurrrfectImages
+//
+//  Created by Eslam Nahel on 2023-09-27.
+//
+
+import SwiftUI
+
+@main
+struct PurrrfectImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
